53c591257b97b9e66b092eb3f4b643752ed43eac
torbuntu <sorensentor@tuta.io>
2021-08-05 18:53:44 -0500

    using crops in Garden stage. Adding UI

--- code/entities/Crop.java
+++ code/entities/Crop.java
@@ -1,43 +1,98 @@
 package code.entities;
 
+import femto.mode.HiRes16Color;
+
 // TODO: Make these sprites
-// import sprites.Turnip;
-// import sprites.Radish;
-// import sprites.Daisy;
-// import sprites.Coiffee;
-// import sprites.Tea;
-// import sprites.GreenBean;
-// import sprites.Tomato;
-// import sprites.Cranberry;
-// import sprites.Blueberry;
-// import sprites.MagicFruit;
+import sprites.Turnip;
+import sprites.Radish;
+import sprites.Daisy;
+import sprites.Coffee;
+import sprites.Tea;
+import sprites.GreenBean;
+import sprites.Tomato;
+import sprites.Blueberry;
+import sprites.MagicFruit;
 
 public class Crop {
     int type, growth, water, x, y;
-    
-    // Turnip turnip;
-    // Radish radish;
-    // Daisy daisy;
-    // Coffee coffee;
-    // Tea tea;
-    // GreenBean greenBean;
-    // Tomato tomato;
-    // Cranberry cranberry;
-    // Blueberry blueberry;
-    // MagicFruit magicFruit;
+    int renderX, renderY;
+    Turnip turnip;
+    Radish radish;
+    Daisy daisy;
+    GreenBean greenBean;
+    Coffee coffee;
+    Tea tea;
+
+    Tomato tomato;
+    Blueberry blueberry;
+    MagicFruit magicFruit;
     
     public Crop(int type, int growth, int x, int y){
         this.type = type;
         this.growth = growth;
         this.x = x;
         this.y = y;
+        this.renderX = 20+x*20;
+        this.renderY = 48+y*16;
+        
+        
         water = 0;
+        initCrop();
+    }
+    
+    private void initCrop(){
+        switch(type){
+            case 0:
+                turnip = new Turnip();
+                turnip.harvest();
+                break;
+            case 1:
+                radish = new Radish();
+                radish.harvest();
+                break;
+            case 2:
+                daisy = new Daisy();
+                daisy.harvest();
+                break;
+            case 3:
+                greenBean = new GreenBean();
+                greenBean.harvest();
+                break;
+        }
+    }
+    
+    public void plant(int type){
+        this.type = type;
+        growth = 0;
+        initCrop();
     }
     
     
-    void update(){}
+    void update(){
+
+    }
     
-    void render(){}
+    void render(HiRes16Color screen){
+        // TODO: draw numbers temporarily.
+        screen.setTextColor(1);
+        screen.setTextPosition(2+renderX, 6+renderY);
+        screen.print(type + "," + growth);
+        switch(type){
+            case 0:
+                turnip.draw(screen,renderX,renderY);
+                break;
+            case 1:
+                radish.draw(screen,renderX,renderY);
+                break;
+            case 2:
+                daisy.draw(screen,renderX,renderY);
+                break;
+            case 3:
+                greenBean.draw(screen, renderX, renderY);
+                break;
+        }
+    }
+
     
     int getGrowth(){return growth;}
     int getType(){return type;}
--- code/stages/Garden.java
+++ code/stages/Garden.java
@@ -21,11 +21,17 @@ class Garden extends State {
     
     Crop[] crops;
     
+    int dayProgress, cursor;
+    
+    boolean pause = false;
+    
     void init(){
         screen = Globals.screen;
         player = new Player();
         byte[] field = Globals.load("field");
-
+        dayProgress = 0;
+        cursor = 0;
+        
         // TODO: Fix this nonsense.
         crops = new Crop[36];
         int y = 0;
@@ -33,10 +39,7 @@ class Garden extends State {
         int id = 0;
         for(int i =0; i < 72; i+=2){
             if(id > 35)return;
-            crops[id] = new Crop(
-                field[i],
-                field[i+1], 
-                x, y);
+            crops[id] = new Crop( field[i], field[i+1], x, y );
             x++;
             if(x > 5){
                 x=0;
@@ -52,9 +55,19 @@ class Garden extends State {
         screen = null;
         player.dispose();
         player = null;
+        crops = null;
     }
     
     void update(){
+        if(pause){
+            handheldMenu();
+            
+            // We don't want to progress with any other details while in the handheld menu.
+            return;
+        }
+        dayProgress++;
+        
+        
         if(Button.A.justPressed()){
 
             byte[] save = new byte[crops.length * 2];
@@ -70,13 +83,16 @@ class Garden extends State {
         }
         
         if( Button.B.justPressed() ){
-            int x = (player.x - 20)/20;
-            int y = (player.y - 64)/16;
-            System.out.println("Affected tile: " + player.x + ","+player.y);
-            System.out.print("X: " + x);
-            System.out.print(", Y: " + y);
-            crops[x+y*6].type = 5;
-            crops[x+y*6].growth = 0;
+            if(player.x >= 20 && player.x < 140 
+            && player.y >= 64 && player.y < 160){
+                int x = (player.x - 20)/20;
+                int y = (player.y - 64)/16;
+                crops[x+y*6].plant(player.inventory.equippedSeed);
+            }
+        }
+        
+        if( Button.C.justPressed() ){
+            pause = !pause;
         }
         
         // handle movement
@@ -94,23 +110,79 @@ class Garden extends State {
         }
         
         // Render
-        screen.clear(3);
-        
-        // crops
-        for(Crop c:crops){
-            screen.drawRect(20+c.getX()*20, 64+c.getY()*16, 20, 16, c.getType()+5);
-        }
+        screen.clear(7);
         
         // pond
         screen.fillRect(0,0, 50,36, 14);
-        screen.fillRect(36, 18, 30, 16, 7);
+        screen.fillRect(36, 18, 30, 16, 8);
         
         // house
         screen.fillRect(140, 0, 80, 32, 4);
         screen.fillRect(130, 0, 90, 8, 12);
         
+        // crops
+        for(Crop c:crops){
+            screen.drawRect(20+c.getX()*20, 64+c.getY()*16, 20, 16, c.getType()+5);
+            if(c.renderY < player.getRenderY())c.render(screen);
+        }
+        
+        // Render player in between plants that are over or under.
         player.render(screen);
         
+        for(Crop c:crops){
+            if(c.renderY > player.getRenderY()) c.render(screen);
+        }
+        
+        // Render the HUD (equipped item, seed amount, Monies);
+        screen.drawRect(screen.width()-40, screen.height()-16, 20, 16, 3);
+        screen.drawRect(screen.width()-20, screen.height()-16, 20, 16, 3);
+        screen.setTextColor(1);
+        screen.setTextPosition(screen.width()-38, screen.height()-16);
+        screen.print("S");
+        screen.setTextPosition(screen.width()-18, screen.height()-16);
+        
+        screen.print(player.inventory.equippedSeed);
+        
+        // Render day progression
+        screen.drawHLine(0, screen.height()-1, dayProgress, 12);
+        
+        screen.flush();
+    }
+    
+    void handheldMenu(){
+        screen.fillRect(30, 30, screen.width()-60, screen.height()-60, 9);
+        
+        screen.fillRect(15, 60, screen.width()-30, 20, 9);
+        
+        screen.fillRect(40, 40, screen.width()-80, screen.height()-80, 5);
+        screen.fillRect(42, 42, screen.width()-84, screen.height()-84, 1);
+        screen.setTextColor(10);
+        screen.setTextPosition(43, 43);
+        screen.print("-- Menu --");
+        
+        screen.setTextPosition(45, 43+8);
+        if(cursor == 0)screen.print("> ");
+        screen.print("Seed to plant: " + player.inventory.equippedSeed);
+        
+        if(Button.Up.justPressed())cursor--;
+        if(Button.Down.justPressed())cursor++;
+        if(cursor < 0)cursor = 0;
+        if(cursor > 4)cursor = 4;
+        
+        
+        if(Button.Left.justPressed()){
+            
+        }
+        if(Button.Right.justPressed()){
+            
+        }
+        
+        if(Button.B.justPressed()){
+            player.inventory.equippedSeed++;
+            if(player.inventory.equippedSeed > 9)player.inventory.equippedSeed = 0;
+        }
+        
+        if(Button.C.justPressed())pause=false;
         
         screen.flush();
     }
